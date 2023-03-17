c968726b9965b1a767f13fb881530b9edef76edf
manasa <manasa.busireddy@gmail.com>
2023-03-17 19:23:49 -0400

    first code

--- ec2/main.tf
+++ ec2/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "ec2" {
       password = "DevOps321"
     }
     inline = [
-      "git clone https://github.com/raghudevopsb71/roboshop.shell"
+      "git clone https://github.com/raghudevopsb71/roboshop.shell",
       "cd roboshop-shell",
       "sudo bach ${var.component}.sh"
     ]
