8eff8730046b0f0042e062aef22d5c4b3c549c01
manasa <manasa.busireddy@gmail.com>
2023-03-17 20:29:30 -0400

    first code

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "ec2" {
   source = "./ec2"
   component = each.value["name"]
   instance_type = each.value["type"]
-  password = each.value["password"]
+  password = try(each.value["password"],null)
 }
 
 
